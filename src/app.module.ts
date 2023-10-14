1fbb1cc1a08a8274abd1d2ea3b351efdcd48c838
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-10-14 21:45:19 +0900

    feat: get token / signup API

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
       synchronize: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,66 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import User from 'src/entities/user.entity';
-import { Repository } from 'typeorm';
+import { Injectable, Logger } from '@nestjs/common';
+import { UnprocessableEntityException } from '@nestjs/common/exceptions';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { JwtPayload, SignupPayload } from 'src/interfaces/auth';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
   ) {}
 
-  async validateUser() {}
+  private readonly logger = new Logger(AuthService.name);
+
+  async getRefreshToken({ id }) {
+    const payload: JwtPayload = {
+      id: id,
+      signedAt: new Date().toISOString(),
+    };
+
+    const refreshJwt = await this.jwtService.sign(payload, {
+      secret: process.env.REFRESH_TOKEN_SECRET_KEY,
+    });
+    this.userService.updateUser(id, 'token', refreshJwt);
+
+    return refreshJwt;
+  }
+
+  async getAccessToken({ id }) {
+    const payload: JwtPayload = {
+      id: id,
+      signedAt: new Date().toISOString(),
+    };
+
+    const accessJwt = await this.jwtService.sign(payload, {
+      secret: process.env.ACCESS_TOKEN_SECRET_KEY,
+      expiresIn: '1h',
+    });
+
+    return accessJwt;
+  }
+
+  async getSignupToken(signupPayload: SignupPayload) {
+    const payload: SignupPayload = {
+      id: signupPayload.id,
+      infoId: signupPayload.infoId,
+      page: signupPayload.page + 1,
+    };
+
+    const signupJwt = await this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET_KEY,
+    });
+
+    return signupJwt;
+  }
+
+  async validateUser(id: number) {
+    const user = await this.userService.findUser('id', id);
+
+    if (!user) {
+      throw new UnprocessableEntityException('등록되지 않은 사용자입니다.');
+    }
+    return user;
+  }
 }
