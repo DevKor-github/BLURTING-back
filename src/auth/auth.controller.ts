ca2f12c3ef3ab95f50f848f04ecb8b2a1c3155fd
overthestream <njhnjh02@naver.com>
2023-10-22 22:10:10 +0900

    pull main

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,5 +3,76 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @UseGuards(SignupGuard)
+  @Post('/signup')
+  async signup(
+    @Req() req: Request,
+    @Body() info: CreateUserDto,
+    @Res() res: Response,
+  ) {
+    const { id, infoId, page } = req.user as SignupPayload;
+    if (page > 15) {
+      // email, phoneNumber 저장
+      return res.json({
+        refreshToken: await this.authService.getRefreshToken({
+          id: id,
+        }),
+        accessToken: await this.authService.getAccessToken({
+          id: id,
+        }),
+      });
+    }
+
+    const pageName = Object.keys(Page).find((key) => Page[key] == page);
+
+    switch (pageName) {
+      case 'email':
+        // email 인증
+        break;
+      case 'phoneNumber':
+        // phone 인증
+        break;
+      case 'userName':
+        this.userService.updateUser(id, 'userName', info['userName']);
+        break;
+      default:
+        this.userService.updateUserInfo(infoId, pageName, info[pageName]);
+    }
+
+    const signupToken = await this.authService.getSignupToken(
+      req.user as SignupPayload,
+    );
+
+    return res.json({ signupToken: signupToken });
+  }
+
+  @Post('/login')
+  async login(@Body() loginDto: LoginDto, @Res() res: Response) {
+    const { id } = loginDto;
+
+    const user = await this.authService.validateUser(id);
+    const refreshToken = await this.authService.getRefreshToken({
+      id: user.id,
+    });
+    const accessToken = await this.authService.getAccessToken({ id: user.id });
+    return res.json({
+      refreshToken: refreshToken,
+      accessToken: accessToken,
+    });
+  }
+
+  @UseGuards(AuthGuard('refresh'))
+  @Post('/refresh')
+  async refresh(@Req() req: Request, @Res() res: Response) {
+    const { id } = req.user as JwtPayload;
+    const accessToken = await this.authService.getAccessToken({ id: id });
+    return res.json({
+      accessToken: accessToken,
+    });
+  }
 }
---
+++ src/s3/s3.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Inject } from '@nestjs/common';
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  PutObjectCommandInput,
+  ObjectCannedACL,
+} from '@aws-sdk/client-s3';
+
+@Injectable()
+export class S3Service {
+  constructor(@Inject('S3Client') private readonly s3: S3Client) {}
+
+  async uploadImage(file: Express.Multer.File) {
+    const key = `${Date.now() + file.originalname}`;
+    const params: PutObjectCommandInput = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      ACL: process.env.AWS_ACL as ObjectCannedACL,
+      Key: key,
+      Body: file.buffer,
+    };
+
+    await this.s3.send(new PutObjectCommand(params));
+    return key;
+  }
+
+  async deleteImage(key: string) {
+    const params: PutObjectCommandInput = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+    };
+
+    await this.s3.send(new DeleteObjectCommand(params));
+  }
+}
