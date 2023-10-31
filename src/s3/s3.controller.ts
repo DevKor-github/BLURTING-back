51bdbe957304883b35f7088176f55ff904bddb1b
overthestream <njhnjh02@naver.com>
2023-10-31 18:22:09 +0900

    docs: s3 api

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -6,20 +6,39 @@ import {
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { S3Service } from './s3.service';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FileUploadDto, FileUploadResponseDto } from './dtos/fileUploadDto';
 
+@ApiTags('s3')
 @Controller('s3')
 export class S3Controller {
   constructor(private readonly s3Service: S3Service) {}
 
   @Post('')
   @UseInterceptors(FilesInterceptor('files', 10))
-  async uploadFiles(@UploadedFiles() files) {
-    const imgurl: string[] = [];
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: '이미지 파일 리스트',
+    type: FileUploadDto,
+  })
+  @ApiCreatedResponse({
+    description: '이미지 업로드 성공',
+    type: [FileUploadResponseDto],
+  })
+  @ApiBadRequestResponse({ description: '이미지 업로드 실패' })
+  async uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    const imgurl: FileUploadResponseDto[] = [];
     await Promise.all(
       files.map(async (file: Express.Multer.File) => {
         const key = await this.s3Service.uploadImage(file);
         const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
-        imgurl.push(fileUrl);
+        imgurl.push({ url: fileUrl });
       }),
     );
 
