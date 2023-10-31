b2bf9f9437541cf1fd108957f092411c762ab857
overthestream <njhnjh02@naver.com>
2023-10-31 18:36:52 +0900

    docs: geocoding

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('health-check')
+  @ApiResponse({
+    status: 200,
+    description: 'health check',
+    type: 'Hello World!',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/geocoding/geocoding.controller.ts
+++ src/geocoding/geocoding.controller.ts
@@ -1,15 +1,37 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { GeocodingService } from './geocoding.service';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('geocoding')
+@ApiTags('geocoding')
 export class GeocodingController {
   constructor(private readonly geocodingService: GeocodingService) {}
 
   @Get('/search/district/by-name')
+  @ApiQuery({
+    name: 'name',
+    description: '검색할 지역 이름',
+    type: String,
+    example: '성북구',
+  })
+  @ApiOkResponse({
+    description: '검색한 지역 이름에 대한 지역 리스트',
+    type: [String],
+  })
   async getDistrictListByName(@Query('name') name: string) {
     return await this.geocodingService.searchDistrictByName(name);
   }
 
+  @ApiQuery({
+    name: 'geo',
+    description: '검색할 지역 위도, 경도',
+    type: String,
+    example: 'POINT(127.0164 37.4984)',
+  })
+  @ApiOkResponse({
+    description: '검색한 좌표에 대한 지역 리스트',
+    type: [String],
+  })
   @Get('/search/district/by-geo')
   async getDistrictListByGeo(@Query('geo') geo: string) {
     return await this.geocodingService.searchDistrictByGeo(geo);
