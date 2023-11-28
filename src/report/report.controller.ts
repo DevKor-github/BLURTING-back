cfbb309c928e42a92728be43adf7f6c9bff788c1
overthestream <njhnjh02@naver.com>
2023-11-28 18:44:51 +0900

    신고 APi

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req } from '@nestjs/common';
+import { ReportService } from './report.service';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { JwtPayload } from 'src/interfaces/auth';
+import { ReportingRequestDto } from './dtos/reportingRequest.dto';
 
 @Controller('report')
-export class ReportController {}
+@ApiTags('report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post('/:id')
+  @ApiOperation({
+    summary: '블러팅 신고',
+  })
+  @ApiParam({
+    name: 'id',
+    description: '신고할 유저의 id',
+    required: true,
+    type: Number,
+  })
+  @ApiBody({ type: ReportingRequestDto, description: '신고 사유' })
+  async reportUser(
+    @Param('id') reportingId: number,
+    @Req() req: Request,
+    @Body() body: ReportingRequestDto,
+  ) {
+    const { id } = req.user as JwtPayload;
+    await this.reportService.reportUser(id, reportingId, body.reason);
+  }
+}
--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportEntity } from 'src/entities/report.entity';
 
 @Module({
   providers: [ReportService],
-  controllers: [ReportController]
+  controllers: [ReportController],
+  imports: [TypeOrmModule.forFeature([ReportEntity])],
 })
 export class ReportModule {}
