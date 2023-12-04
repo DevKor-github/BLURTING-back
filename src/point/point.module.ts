37d92eb376ee5fcff271793d46cd165e263718fd
kyeah <39901387+dmobtxx19@users.noreply.github.com>
2023-12-04 23:54:59 +0900

    feat: ban create room when reported

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -4,11 +4,13 @@ import { PointHistoryEntity, UserEntity } from 'src/entities';
 import { PointController } from './point.controller';
 import { PointService } from './point.service';
 import { ChatModule } from 'src/chat/chat.module';
+import { ReportModule } from 'src/report/report.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity, PointHistoryEntity]),
     ChatModule,
+    ReportModule,
   ],
   controllers: [PointController],
   providers: [PointService],
