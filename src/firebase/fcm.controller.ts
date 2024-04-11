ca77669a838a3f21c88c49a04c81fee65855bfad
overthestream <njhnjh02@naver.com>
2024-04-12 02:48:25 +0900

    guard 변경

--- src/firebase/fcm.controller.ts
+++ src/firebase/fcm.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { NotificationListDto } from './dtos/notificationList.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { FcmService } from './fcm.service';
 import { JwtPayload } from 'src/interfaces/auth';
 import { User } from 'src/decorators/accessUser.decorator';
 import { Docs } from 'src/decorators/swagger/fcm.decorator';
+import { AccessGuard } from 'src/auth/guard/acces.guard';
 
 @Controller('fcm')
 @ApiTags('fcm')
@@ -13,7 +13,7 @@ export class fcmController {
   constructor(private readonly fcmService: FcmService) {}
 
   @Get()
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(AccessGuard)
   @Docs('getNotificationList')
   async getNotificationList(
     @User() user: JwtPayload,
@@ -22,14 +22,14 @@ export class fcmController {
   }
 
   @Get('/disable')
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(AccessGuard)
   @Docs('disableNotification')
   async disableNotification(@User() user: JwtPayload): Promise<void> {
     await this.fcmService.disableNotification(user.id);
   }
 
   @Get('/check')
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(AccessGuard)
   @Docs('checkNotification')
   async checkNotification(@User() user: JwtPayload): Promise<boolean> {
     return await this.fcmService.checkNotification(user.id);
