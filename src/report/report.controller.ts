2a8c6f43583fc1a9569b31e479617a4cc5c19f68
kyeah <dmobtxx19@gmail.com>
2024-04-28 20:21:15 +0900

    refactor(report): apply decorator docs

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ReportService } from './report.service';
-import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
-import { Request } from 'express';
+import { ApiTags } from '@nestjs/swagger';
 import { JwtPayload } from 'src/interfaces/auth';
 import { ReportingRequestDto } from './dtos/reportingRequest.dto';
+import { ReportDocs } from 'src/decorators/swagger/report.decorator';
+import { User } from 'src/decorators/accessUser.decorator';
 
 @Controller('report')
 @ApiTags('report')
@@ -11,22 +12,13 @@ export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
   @Post('/:id')
-  @ApiOperation({
-    summary: '블러팅 신고',
-  })
-  @ApiParam({
-    name: 'id',
-    description: '신고할 유저의 id',
-    required: true,
-    type: Number,
-  })
-  @ApiBody({ type: ReportingRequestDto, description: '신고 사유' })
+  @ReportDocs()
   async reportUser(
     @Param('id') reportingId: number,
-    @Req() req: Request,
+    @User() userPayload: JwtPayload,
     @Body() body: ReportingRequestDto,
   ) {
-    const { id } = req.user as JwtPayload;
+    const { id } = userPayload;
     await this.reportService.reportUser(id, reportingId, body.reason);
   }
 }
