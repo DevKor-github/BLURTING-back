2dc0e9a549b945113686b1caa057a40f6f5b0d68
kyeah <dmobtxx19@gmail.com>
2024-05-01 14:30:33 +0900

    fix(home): dto validator

--- src/home/dtos/likeHomeAnswer.dto.ts
+++ src/home/dtos/likeHomeAnswer.dto.ts
@@ -1,5 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class likeHomeAnswerDto {
-  @ApiProperty({ description: 'answer id' }) answerId: number;
+  @IsNumber()
+  @ApiProperty({ description: 'answer id' })
+  answerId: number;
 }
