6550b1470095921729c3e382472f6ff8f35848f9
overthestream <njhnjh02@naver.com>
2024-06-05 16:04:22 +0900

    add docs

--- src/domain/validation/validation.controller.ts
+++ src/domain/validation/validation.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { ValidationService } from './validation.service';
+import { AccessGuard } from '../auth/guard/access.guard';
+import { ValidationDocs } from 'src/decorators/swagger/validation.decorator';
 
 @Controller('validation')
 @ApiTags('validation')
 export class ValidationController {
-  constructor() {}
+  constructor(private readonly validationService: ValidationService) {}
 
   @Get('/purchase/:productId/:token')
-  async purchaseValidation() {
-    return 'purchaseValidation';
+  @UseGuards(AccessGuard)
+  @ValidationDocs('purchaseValidation')
+  async purchaseValidation(
+    @Param('productId') productId: string,
+    @Param('token') token: string,
+  ) {
+    this.validationService.validatePurchase(productId, token);
+  }
+
+  @Get('/admob')
+  @ValidationDocs('admobValidation')
+  async admobValidation(@Req() req: Request) {
+    this.validationService.validateAdMob(req.url);
   }
 }
