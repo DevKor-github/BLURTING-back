e98af4694ca6eb21d0c245ac4f1b609c21991d9b
overthestream <njhnjh02@naver.com>
2024-08-18 23:11:18 +0900

    feat/apple

--- src/domain/validation/validation.service.ts
+++ src/domain/validation/validation.service.ts
@@ -1,17 +1,27 @@
+import crypto from 'crypto';
+import type {
+  JWSTransactionDecodedPayload,
+  TransactionInfoResponse,
+} from '@apple/app-store-server-library';
 import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
-import axios from 'axios';
-import crypto from 'crypto';
-
-import { ProductPurchase } from 'src/interfaces/productPurchase';
+import { InjectRepository } from '@nestjs/typeorm';
+import axios, { type AxiosResponse } from 'axios';
+import jwt from 'jsonwebtoken';
+import { AppStoreConnectTransactionEntity } from 'src/domain/entities';
+import type { ProductPurchase } from 'src/interfaces/productPurchase';
+import type { Repository } from 'typeorm';
 
 @Injectable()
 export class ValidationService {
-  constructor() {}
-
+  constructor(
+    @InjectRepository(AppStoreConnectTransactionEntity)
+    private readonly appStoreConnectTransactionRepository: Repository<AppStoreConnectTransactionEntity>,
+  ) {}
   private GOOGLE_AD_KEY_URL =
     'https://gstatic.com/admob/reward/verifier-keys.json';
 
@@ -97,13 +107,88 @@ export class ValidationService {
       verifier.update(contentToVerify);
       const result = verifier.verify(publicKey, signature, 'base64');
       if (result) return true;
-      else throw new BadRequestException('Invalid signature');
-    } else throw new BadRequestException('Invalid key id');
+      throw new BadRequestException('Invalid signature');
+    }
+    throw new BadRequestException('Invalid key id');
   }
 
   async validateAdMob(queryUrl: string) {
     const debug = false;
     await this.verify(queryUrl, debug);
     // TODO: give reward
+  }
+
+  async signAppStoreConnectJwt() {
+    const header = {
+      alg: 'ES256',
+      kid: process.env.APP_STORE_KID,
+      type: 'JWT',
+    };
+    const now = Math.floor(Date.now() / 1000);
+    const payload = {
+      iss: process.env.APP_STORE_ISSURE_ID,
+      iat: now,
+      exp: now + 60 * 30,
+      aud: 'appstoreconnect-v1',
+      bid: process.env.APP_STORE_BUNDLE_ID,
+    };
+    const secret = process.env.APP_STORE_PK;
+
+    return jwt.sign(payload, secret, { algorithm: 'ES256', header });
+  }
+
+  decodeAppStoreConnectJwt(token: string) {
+    const secret = process.env.APP_STORE_PK;
+
+    return jwt.verify(token, secret, {
+      algorithms: ['ES256'],
+      complete: false,
+    });
+  }
+
+  async getTransactionInfo(transactionId: string) {
+    const jwt = this.signAppStoreConnectJwt();
+    const url = `https://api.storekit.itunes.apple.com/inApps/v1/transactions/${transactionId}`;
+    let response: AxiosResponse;
+    try {
+      response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+    } catch (err) {
+      throw new NotFoundException('transaction not found');
+    }
+    const transactionInfoResponse = response.data as TransactionInfoResponse;
+    const info = JSON.parse(
+      this.decodeAppStoreConnectJwt(
+        transactionInfoResponse.signedTransactionInfo,
+      ) as string,
+    ) as JWSTransactionDecodedPayload;
+    return info;
+  }
+
+  async takePointFromTransaction(transactionId: string) {
+    const info = await this.getTransactionInfo(transactionId);
+    const transaction = await this.appStoreConnectTransactionRepository.findOne(
+      {
+        where: { transactionId: info.transactionId },
+      },
+    );
+    if (!transaction) {
+      const newTransaction = this.appStoreConnectTransactionRepository.create({
+        transactionId: info.transactionId,
+        price: info.price,
+        isDone: true,
+      });
+      await this.appStoreConnectTransactionRepository.save(newTransaction);
+    } else if (transaction.isDone) {
+      throw new BadRequestException('already taked point');
+    } else {
+      transaction.isDone = true;
+      await this.appStoreConnectTransactionRepository.save(transaction);
+    }
+    //TODO: take point
+    return 200;
   }
 }
