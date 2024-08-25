ca29997b58270b6b91bc1a123b74ee79e8c21635
overthestream <njhnjh02@naver.com>
2024-08-25 16:01:54 +0900

    log

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,13 @@ async function bootstrap() {
     }),
   );
   console.log(process.env.MONGO_URI);
+  console.log(
+    process.env.DB_HOST,
+    process.env.DB_PORT,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    process.env.DB_DATABASE,
+  );
 
   await app.listen(3081);
 }
